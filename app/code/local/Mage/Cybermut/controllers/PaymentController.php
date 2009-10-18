008cbab8a31cc84d3f9cf8dec4a827ea9046bc1a
tharibo@gmail.com <tharibo@gmail.com@658ad160-ab48-11de-ac05-951101a8bf3e>
2009-10-18 18:28:35 +0000

    Ajout des fichier Cybermut

---
+++ app/code/local/Mage/Cybermut/controllers/PaymentController.php
@@ -0,0 +1,273 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category   Mage
+ * @package    Mage_Cybermut
+ * @copyright  Copyright (c) 2004-2007 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Cybermut Payment Front Controller
+ *
+ * @category   Mage
+ * @package    Mage_Cybermut
+ * @name       Mage_Cybermut_PaymentController
+ * @author	   Magento Core Team <core@magentocommerce.com>, Quadra Informatique - Nicolas Fischer <nicolas.fischer@quadra-informatique.fr>
+ */
+class Mage_Cybermut_PaymentController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Order instance
+     */
+    protected $_order;
+
+    /**
+     *  Get order
+     *
+     *  @param    none
+     *  @return	  Mage_Sales_Model_Order
+     */
+    public function getOrder()
+    {
+        if ($this->_order == null) {
+            $session = Mage::getSingleton('checkout/session');
+            $this->_order = Mage::getModel('sales/order');
+            $this->_order->loadByIncrementId($session->getLastRealOrderId());
+        }
+        return $this->_order;
+    }
+
+    /**
+     * When a customer chooses Cybermut on Checkout/Payment page
+     *
+     */
+	public function redirectAction()
+	{
+		$session = Mage::getSingleton('checkout/session');
+		$session->setCybermutPaymentQuoteId($session->getQuoteId());
+
+		$order = $this->getOrder();
+
+		if (!$order->getId()) {
+			$this->norouteAction();
+			return;
+		}
+
+		$order->addStatusToHistory(
+			$order->getStatus(),
+			Mage::helper('cybermut')->__('Customer was redirected to Cybermut')
+		);
+		$order->save();
+
+		$this->getResponse()
+			->setBody($this->getLayout()
+				->createBlock('cybermut/redirect')
+				->setOrder($order)
+				->toHtml());
+
+        $session->unsQuoteId();
+    }
+
+	/**
+	 *  Cybermut response router
+	 *
+	 *  @param    none
+	 *  @return	  void
+	 */
+	public function notifyAction()
+	{
+		$model = Mage::getModel('cybermut/payment');
+        
+        if ($this->getRequest()->isPost()) {
+			$postData = $this->getRequest()->getPost();
+        	$method = 'post';
+
+		} else if ($this->getRequest()->isGet()) {
+			$postData = $this->getRequest()->getQuery();
+			$method = 'get';
+
+		} else {
+			$model->generateErrorResponse();
+		}
+
+		$returnedMAC = $postData['MAC'];
+		$correctMAC = $model->getResponseMAC($postData);
+
+		if ($model->getConfigData('debug_flag')) {
+			Mage::getModel('cybermut/api_debug')
+				->setResponseBody(print_r($postData ,1))
+				->save();
+		}
+
+		$order = Mage::getModel('sales/order')
+			->loadByIncrementId($postData['reference']);
+
+		if (!$order->getId()) {
+			$model->generateErrorResponse();
+		}
+
+		if ($returnedMAC == $correctMAC) {
+			if ($model->isSuccessfulPayment($postData['code-retour'])) {
+				$order->addStatusToHistory(
+					$model->getConfigData('order_status_payment_accepted'),
+					Mage::helper('cybermut')->__('Payment accepted by Cybermut'),
+					true
+				);
+				
+				$order->sendNewOrderEmail();
+				$order->setEmailSent(true);
+
+				if ($this->saveInvoice($order)) {
+//                $order->setState(Mage_Sales_Model_Order::STATE_PROCESSING, true);
+				}
+				
+			 } else {
+			 	$order->addStatusToHistory(
+					$model->getConfigData('order_status_payment_refused'),
+					Mage::helper('cybermut')->__('Payment refused by Cybermut')
+				);
+
+				if ($model->getConfigData('order_status_payment_refused') == Mage_Sales_Model_Order::STATE_CANCELED) {
+					$order->cancel();
+				}
+				
+				// TODO: customer notification on payment failure
+			 }
+				
+			$order->save();
+			if ($method == 'post') {
+				$model->generateSuccessResponse();
+			} else if ($method == 'get') {
+				return;
+			}
+
+        } else {
+            $order->addStatusToHistory(
+                $order->getStatus(),
+                Mage::helper('cybermut')->__('Returned MAC is invalid. Order cancelled.')
+            );
+            $order->cancel();
+            $order->save();
+            $model->generateErrorResponse();
+        }
+    }
+
+    /**
+     *  Save invoice for order
+     *
+     *  @param    Mage_Sales_Model_Order $order
+     *  @return	  boolean Can save invoice or not
+     */
+    protected function saveInvoice(Mage_Sales_Model_Order $order)
+    {
+        if ($order->canInvoice()) {
+            
+            $version = Mage::getVersion();
+            $version = substr($version, 0, 5);
+            $version = str_replace('.', '', $version);
+            while (strlen($version) < 3) {
+            	$version .= "0";
+            }
+            
+            if (((int)$version) < 111) {
+	            $convertor = Mage::getModel('sales/convert_order');
+	            $invoice = $convertor->toInvoice($order);
+	            foreach ($order->getAllItems() as $orderItem) {
+	               if (!$orderItem->getQtyToInvoice()) {
+	                   continue;
+	               }
+	               $item = $convertor->itemToInvoiceItem($orderItem);
+	               $item->setQty($orderItem->getQtyToInvoice());
+	               $invoice->addItem($item);
+	            }
+	            $invoice->collectTotals();
+
+            } else {
+            	$invoice = $order->prepareInvoice();
+			}
+
+			$invoice->register()->capture();
+            Mage::getModel('core/resource_transaction')
+               ->addObject($invoice)
+               ->addObject($invoice->getOrder())
+               ->save();
+            return true;
+        }
+
+        return false;
+    }
+
+	/**
+	 *  Success payment page
+	 *
+	 *  @param    none
+	 *  @return	  void
+	 */
+	public function successAction()
+	{
+		$session = Mage::getSingleton('checkout/session');
+		$session->setQuoteId($session->getCybermutPaymentQuoteId());
+		$session->unsCybermutPaymentQuoteId();
+		
+		$order = $this->getOrder();
+		
+		if (!$order->getId()) {
+			$this->norouteAction();
+			return;
+		}
+
+		$order->addStatusToHistory(
+			$order->getStatus(),
+			Mage::helper('cybermut')->__('Customer successfully returned from Cybermut')
+		);
+        
+		$order->save();
+        
+		$this->_redirect('checkout/onepage/success');
+	}
+
+	/**
+	 *  Failure payment page
+	 *
+	 *  @param    none
+	 *  @return	  void
+	 */
+	public function errorAction()
+	{
+        $session = Mage::getSingleton('checkout/session');
+        $model = Mage::getModel('cybermut/payment');
+
+        $order = $this->getOrder();
+
+        if (!$order->getId()) {
+            $this->norouteAction();
+            return;
+        }
+        if ($order instanceof Mage_Sales_Model_Order && $order->getId()) {
+            $order->addStatusToHistory(
+                $model->getConfigData('order_status_payment_refused'),
+                Mage::helper('cybermut')->__('Customer returned from Cybermut.')
+            );
+
+			if ($model->getConfigData('order_status_payment_refused') == Mage_Sales_Model_Order::STATE_CANCELED) {
+				$order->cancel();
+			}
+            
+            $order->save();
+        }
+        
+        $this->_redirect('checkout/cart');
+    }
+}
